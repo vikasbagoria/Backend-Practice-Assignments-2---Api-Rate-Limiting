5b988c1ec8425ba369fa69f8254ab11af066c5f1
vikasbagoria <vikasbagoria@gmail.com>
2021-06-22 15:53:32 +0530

    try

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
+const data = require("./initialData");
 const port = 3000
 app.use(express.urlencoded());
 
@@ -12,6 +13,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
+
+app.get('/api/posts', (req, res) => {
+    var max =req.query.max;
+    var limit =0;
+
+    if(max>data.length){
+        limit = data.slice(0, 9) 
+    }else{
+   limit = data.slice(0,max || 9)
+    }
+    // console.log(max);
+    res.status(200).send(limit);
+});
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
